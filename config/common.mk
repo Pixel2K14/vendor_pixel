dde1f36ae16ec3f55bf68bec6c2a7c91d07b887d
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-03-20 21:20:11 +0000

    vendor: revert

--- config/common.mk
+++ config/common.mk
@@ -84,10 +84,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/pixel/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
 
-# SELinux filesystem labels
-PRODUCT_COPY_FILES += \
-    vendor/pixel/prebuilt/common/etc/init.d/50selinuxrelabel:system/etc/init.d/50selinuxrelabel
-
 # CM-specific init file
 PRODUCT_COPY_FILES += \
     vendor/pixel/prebuilt/common/etc/init.local.rc:root/init.cm.rc
