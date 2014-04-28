2e09e4911b658cd1502c1e3ee5b759369fac3221
Clark Scheff <clark@cyngn.com>
2014-04-28 12:03:07 -0700

    CM11 Themes: Add feature for new themes.

--- config/themes_common.mk
+++ config/themes_common.mk
@@ -5,4 +5,5 @@ PRODUCT_PACKAGES += \
        com.tmobile.themes
 
 PRODUCT_COPY_FILES += \
-       vendor/pixel/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
+       vendor/pixel/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+       vendor/pixel/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
