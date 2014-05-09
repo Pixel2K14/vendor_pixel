c4cdfd67a40c62412e000830e60c16b804caf5ba
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-05-09 22:24:12 +0100

    ota: Validate any installed data's signature against our own
    Try to prevent incompatible-signature system quirkiness
    
    Change-Id: I73402d963a1b57ad0ee3c68ced4f2d7f074f927f

--- config/common.mk
+++ config/common.mk
@@ -75,6 +75,10 @@ PRODUCT_COPY_FILES += \
     vendor/pixel/prebuilt/common/bin/blacklist:system/addon.d/blacklist
 endif
 
+# Signature compatibility validation
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/bin/otasigcheck.sh:system/bin/otasigcheck.sh
+
 # init.d support
 PRODUCT_COPY_FILES += \
     vendor/pixel/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
