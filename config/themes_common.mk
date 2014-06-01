dfff2615e6f6fff91c0bdb2979002eaeb8f4adf1
Arnav Gosain <gosain.arnav@gmail.com>
2014-06-01 23:43:36 +0530

    vendor: yeah!!!!!!!!!! FIX THEMES

--- config/themes_common.mk
+++ config/themes_common.mk
@@ -1,8 +1,8 @@
-# T-Mobile theme engine
+# Theme engine
 PRODUCT_PACKAGES += \
-       ThemeChooser \
-       ThemeProvider
+    ThemeChooser \
+    ThemesProvider
 
 PRODUCT_COPY_FILES += \
-       vendor/pixel/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
-       vendor/pixel/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
+    vendor/cm/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+    vendor/cm/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
